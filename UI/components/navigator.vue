7f5b455dc87291118451215c70d4e8481e9ff706
harmyderoman <romangarmider@gmail.com>
2021-07-13 11:51:19 +0300

    toolbar for client

--- UI/components/navigator.vue
+++ UI/components/navigator.vue
@@ -1,29 +1,57 @@
 <template>
-  <v-toolbar dark="isDarkTheme" prominent :src="toolbarImageSrc">
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <div>
+    <v-app-bar dark="isDarkTheme" prominent :src="toolbarImageSrc">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"> </v-app-bar-nav-icon>
 
-    <v-toolbar-title>Conditory</v-toolbar-title>
+      <v-toolbar-title>Conditory</v-toolbar-title>
+      <template v-slot:extension>
+        <v-toolbar-item><v-btn>Home</v-btn></v-toolbar-item>
+        <v-toolbar-item><v-btn>Menu</v-btn></v-toolbar-item>
+      </template>
 
-    <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-    <v-btn icon>
-      <v-icon>mdi-export</v-icon>
-    </v-btn>
-    <!-- <template v-slot:extension>
-      <v-tabs align-with-title>
-        <v-tab>Home</v-tab>
-        <v-tab>Menu</v-tab>
-        <v-tab>Stuff</v-tab>
-        <v-tab>Get direction</v-tab>
-      </v-tabs>
-    </template> -->
-  </v-toolbar>
+      <v-btn icon> <v-icon>mdi-cart-outline</v-icon> </v-btn>
+      <v-btn icon> <v-icon>mdi-account</v-icon> </v-btn>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Menu</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Cart</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title
+              ><a href="/control">Control Panel</a></v-list-item-title
+            >
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script>
   module.exports = {
     data() {
       return {
+        group: null,
+        drawer: false,
         toolbarImageSrc:
           "https://64.media.tumblr.com/c382f41340942888ec22a3c6a76d19ab/tumblr_odzsg8FOoi1ul5fqko1_1280.png"
       }
@@ -31,6 +59,12 @@
     computed: {
       isDarkTheme() {
         return window.app.$vuetify.theme.dark
+      }
+    },
+
+    watch: {
+      group() {
+        this.drawer = false
       }
     }
   }
---
+++ UI/pages/menu.vue
@@ -0,0 +1,94 @@
+<script>
+  // This shit is made for scripting
+  module.exports = {
+    data() {
+      return {
+        desserts: []
+      }
+    },
+    created() {
+      app.apishift.setSubtitle("Menu")
+
+      APIShift.API.request(
+        "Conditory\\Desserts",
+        "getAllDesserts",
+        {},
+        (res) => (this.desserts = res.data)
+      )
+    }
+  }
+</script>
+
+<template>
+  <v-main>
+    <h1>Menu Page!</h1>
+    <div class="grid-container">
+      <!-- <v-layout align-center justify-center> -->
+      <v-card
+        v-for="item in desserts"
+        :key="item.id"
+        :loading="false"
+        class="mx-auto my-12 grid-item"
+        max-width="374"
+      >
+        <template slot="progress">
+          <v-progress-linear
+            color="deep-purple"
+            height="10"
+            indeterminate
+          ></v-progress-linear>
+        </template>
+
+        <v-img height="250" :src="item.image"></v-img>
+
+        <v-card-title>{{ item.name }}</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ms-4">4.5 (413)</div>
+          </v-row>
+
+          <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+
+          <div>
+            Small plates, salads & sandwiches - an intimate setting with 12
+            indoor seats plus patio seating.
+          </div>
+        </v-card-text>
+
+        <v-divider class="mx-4"></v-divider>
+
+        <v-card-title>Tonight's availability</v-card-title>
+
+        <v-card-actions>
+          <v-btn color="deep-purple lighten-2" text> Reserve </v-btn>
+        </v-card-actions>
+      </v-card>
+      <!-- </v-layout> -->
+    </div>
+  </v-main>
+</template>
+
+<style>
+  /* Please style this crap, with style */
+  .grid-container {
+    grid-template-columns: 1fr 1fr 1fr;
+    /* grid-template-rows: 1fr 1fr 1fr; */
+    display: grid;
+    column-gap: 15px;
+    row-gap: 100px;
+    justify-items: center;
+    align-items: center;
+  }
+  .grid-item {
+  }
+</style>
